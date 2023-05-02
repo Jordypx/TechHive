9175006787debe201739e1cec39ab80653643b90
jordypx <jordanodo89@gmail.com>
2023-05-02 11:59:27 -0600

    Swiper

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import ImageSlider from "./ImageSlider";
+
+const Home = () => {
+    return ( 
+        <div className="">
+             <div className="HomeContainer">
+                <ImageSlider />
+             </div>
+        </div>
+     );
+}
+ 
+export default Home;
