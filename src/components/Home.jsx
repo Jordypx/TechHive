b00877b8a705f56ae25711ad7d3024db55389310
jordypx <jordanodo89@gmail.com>
2023-05-02 16:33:05 -0600

    homepage image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,10 @@
-import ImageSlider from "./ImageSlider";
 
 
 const Home = () => {
     return ( 
-        <div className="">
+        <div className="Home">
              <div className="HomeContainer">
-                <ImageSlider/>
+                <img className="w-full" src="https://img.freepik.com/free-vector/cyber-monday-sale-tech-background-online-shopping_1017-28537.jpg?w=740&t=st=1683037689~exp=1683038289~hmac=c3f85c972cd59114f231b802ba6297ab49aed0b1698e3c2f26aa4fc4a432dcdf" alt="" />
              </div>
         </div>
      );
