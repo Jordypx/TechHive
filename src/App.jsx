b01e42a372b3b645d58559222b10b6a67e731208
jordypx <jordanodo89@gmail.com>
2023-05-05 04:45:58 -0600

    devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Home from "./components/Home"
+import Products from "./components/Products"
 
 function App() {
   return (
     <div className="">
       <Header />
       <Home />
+      <Products />
      </div>
   )
 }
---
+++ src/components/Products.jsx
@@ -0,0 +1,98 @@
+const Products = () => {
+    return ( 
+        <div className="">
+                <h1 className="">
+                    Headphones For You!
+                 </h1>
+
+            <div className="headphones-container">
+                 <div className="Image-cards">
+                    <div className="image">
+                        
+                    </div>
+
+                    <div className="content flex">
+                        <div className="description">
+                            <h2>AirPods Max</h2>
+                            <p>A brand new apple airpods max 2021</p>
+                            <div className="reviews">
+
+                                <span className="">
+                                    345
+                                </span>
+                            </div>
+
+                            <div className="addTocart">
+                                <button className="">Add to Cart</button>
+                            </div>
+                        </div>
+
+                        <div className="price">
+                            <span>$250.<i>44</i></span>
+                        </div>
+                    </div>
+                 </div>
+            </div>
+
+            <div className="headphones-container">
+                 <div className="Image-cards">
+                    <div className="image">
+                        
+                    </div>
+
+                    <div className="content flex">
+                        <div className="description">
+                            <h2>AirPods Max</h2>
+                            <p>A brand new apple airpods max 2021</p>
+                            <div className="reviews">
+
+                                <span className="">
+                                    345
+                                </span>
+                            </div>
+
+                            <div className="addTocart">
+                                <button className="">Add to Cart</button>
+                            </div>
+                        </div>
+
+                        <div className="price">
+                            <span>$250.<i>44</i></span>
+                        </div>
+                    </div>
+                 </div>
+            </div>
+
+            <div className="headphones-container">
+                 <div className="Image-cards">
+                    <div className="image">
+                        
+                    </div>
+
+                    <div className="content flex">
+                        <div className="description">
+                            <h2>AirPods Max</h2>
+                            <p>A brand new apple airpods max 2021</p>
+                            <div className="reviews">
+
+                                <span className="">
+                                    345
+                                </span>
+                            </div>
+
+                            <div className="addTocart">
+                                <button className="">Add to Cart</button>
+                            </div>
+                        </div>
+
+                        <div className="price">
+                            <span>$250.<i>44</i></span>
+                        </div>
+                    </div>
+                 </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Products;
