97b1d415f8d51fa0a1feaa4876e697625f917ee8
jordypx <jordanodo89@gmail.com>
2023-05-05 03:32:40 -0600

    home images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,12 @@ const Home = () => {
              <div className="sub-container">
              <div className="container">
               <div className="home-discount">
-                <h1>Grab Upto 25% Off On the<br />Apple Macbook Pro</h1>
+                <h1>Grab Upto 25% Off On the<br />Apple Iphone 13pro max</h1>
                 <button>Buy Now</button>
               </div>
 
               <div className="home-image">
-                <img className="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrP4lXyHlUtsl57lxrk5qyi8JQ_m1Qn0YJaQ&usqp=CAU" alt="" />
+                <img className="" src="https://media.istockphoto.com/id/1383021578/photo/photo-of-young-curly-girl-with-headphones.jpg?s=612x612&w=0&k=20&c=_okMMvroKO4b3K-sXtNoiDkboHoTbRmqjA2TvyfYAyw=" alt="" />
               </div>
              </div>
              </div>
