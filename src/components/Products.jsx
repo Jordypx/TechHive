8048daa1e7eea9d21842d0c5bbe2a1afbccc553f
jordypx <jordanodo89@gmail.com>
2023-05-06 14:48:29 -0600

    images

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,3 +1,5 @@
+import { RiStarSFill } from 'react-icons/ri'
+
 const Products = () => {
     return ( 
         <div className="">
@@ -6,6 +8,42 @@ const Products = () => {
                  </h1>
 
             <div className="headphones-container">
+
+                 <div className="Image-cards">
+                    <div className="image">
+                        <img src="" alt="" className="" />
+                    </div>
+
+                    <div className="content flex">
+                        <div className="description">
+                            <h2>AirPods Max</h2>
+                            <p>A brand new apple airpods max 2021</p>
+                            <div className="reviews flex gap-2">
+                                <div className="stars flex">
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                </div>
+
+                                <span className="">
+                                    345
+                                </span>
+                            </div>
+
+                            <div className="addTocart">
+                                <button className="">Add to Cart</button>
+                            </div>
+                        </div>
+
+                        <div className="price">
+                            <span className=''>$250.<i>44</i></span>
+                        </div>
+                    </div>
+                 </div>
+
+
                  <div className="Image-cards">
                     <div className="image">
                         
@@ -15,7 +53,49 @@ const Products = () => {
                         <div className="description">
                             <h2>AirPods Max</h2>
                             <p>A brand new apple airpods max 2021</p>
-                            <div className="reviews">
+                            <div className="reviews flex gap-2">
+                            <div className="stars flex">
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                </div>
+
+                                <span className="flex gap-2">
+                                    345
+                                </span>
+                            </div>
+
+                            <div className="addTocart">
+                                <button className="">Add to Cart</button>
+                            </div>
+                        </div>
+
+                        <div className="price">
+                            <span>$250.<i>44</i></span>
+                        </div>
+                    </div>
+                 </div>
+           
+
+            <div className="Image-cards">
+                    <div className="image">
+                        
+                    </div>
+
+                    <div className="content flex">
+                        <div className="description">
+                            <h2>AirPods Max</h2>
+                            <p>A brand new apple airpods max 2021</p>
+                            <div className="reviews flex gap-2">
+                            <div className="stars flex">
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                </div>
 
                                 <span className="">
                                     345
@@ -32,9 +112,7 @@ const Products = () => {
                         </div>
                     </div>
                  </div>
-            </div>
 
-            <div className="headphones-container">
                  <div className="Image-cards">
                     <div className="image">
                         
@@ -44,7 +122,14 @@ const Products = () => {
                         <div className="description">
                             <h2>AirPods Max</h2>
                             <p>A brand new apple airpods max 2021</p>
-                            <div className="reviews">
+                            <div className="reviews flex gap-2">
+                            <div className="stars flex">
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                 <RiStarSFill />
+                                </div>
 
                                 <span className="">
                                     345
@@ -61,37 +146,11 @@ const Products = () => {
                         </div>
                     </div>
                  </div>
-            </div>
 
-            <div className="headphones-container">
-                 <div className="Image-cards">
-                    <div className="image">
-                        
-                    </div>
-
-                    <div className="content flex">
-                        <div className="description">
-                            <h2>AirPods Max</h2>
-                            <p>A brand new apple airpods max 2021</p>
-                            <div className="reviews">
-
-                                <span className="">
-                                    345
-                                </span>
-                            </div>
-
-                            <div className="addTocart">
-                                <button className="">Add to Cart</button>
-                            </div>
-                        </div>
-
-                        <div className="price">
-                            <span>$250.<i>44</i></span>
-                        </div>
-                    </div>
                  </div>
+
             </div>
-        </div>
+
      );
 }
  
