c827355b5a8eae47a4db5fff4d40d4112497291a
jordypx <jordanodo89@gmail.com>
2023-05-09 16:16:34 -0600

    update

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -55,7 +55,7 @@ const Checkout = () => {
                              />
                         </div>
 
-                      ksvjhhusoho
+                      ajhuhaguh
 
                     </div>
 
