7af10b97472f437dec1b34ad81fe303732cf970f
jordypx <jordanodo89@gmail.com>
2023-05-09 15:35:02 -0600

    stlying

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,25 +2,26 @@ import Payment from "./Payment";
 import CurrencyFormat from "react-currency-format";
 
 import { RiDeleteBin6Line } from "react-icons/ri";
+import { TbTruckDelivery } from "react-icons/tb"
 
 const Checkout = () => {
     return ( 
         <div className="checkout">
-            <div className="shippingContainer flex">
+            <div className="shippingContainer">
             <div className="review-item">
-                <div className="text">
-                    <h1>Review item And Shipping</h1>
+                    <div className="container-description mt-2 p-9">
+                    <div className="text">
+                    <h1 className="font-semibold text-base mb-4">Review item And Shipping</h1>
                 </div>
-                    <div className="container-description">
 
-                        <div className="image-description flex">
+                        <div className="image-description flex gap-7">
                             <div className="headPhone-image">
                                 <img src="./Max.png" alt="headpphone" />
                             </div>
 
-                            <div className="context">
-                                <h2>Airpods-Max</h2>
-                                <p>Color:<i>Pink</i></p>
+                            <div className=" mt-12">
+                                <h2 className="font-semibold">Airpods-Max</h2>
+                                <p className="font-semibold">Color: <span className="text-sm font-medium">Pink</span></p>
                             </div>
                         </div>
 
@@ -46,6 +47,17 @@ const Checkout = () => {
 
                         <div className="delete-item">
                             <button className="flex items-center">Remove Item <RiDeleteBin6Line/></button>
+                        </div>
+                    </div>
+
+                    <div className="delivery-info">
+                        <div className="icon">
+                            <TbTruckDelivery/>
+                        </div>
+                        <h3>Shipping Information</h3>
+
+                        <div className="delivery-text">
+
                         </div>
                     </div>
             </div>
