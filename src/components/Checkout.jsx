061e2255ad93d88734aa6324438eca1633b4b5ae
jordypx <jordanodo89@gmail.com>
2023-05-10 09:04:55 -0600

    update

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -53,7 +53,7 @@ const Checkout = () => {
                              thousandSeparator={true}
                              prefix={"$"}
 
-                            
+                            nnnnnnnnn
                              
                              />
                         </div>
