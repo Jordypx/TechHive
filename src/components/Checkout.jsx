5742060dda84fa31f442b910cc7b42190b9545f6
jordypx <jordanodo89@gmail.com>
2023-05-10 09:17:15 -0600

    update

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -56,7 +56,7 @@ const Checkout = () => {
                              />
                         </div>
 
-                        wcn wn
+                      
 
                     </div>
                     
