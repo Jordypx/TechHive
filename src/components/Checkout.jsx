eb4faabba989fea1d5ad5980fcfc6dfca682a05f
jordypx <jordanodo89@gmail.com>
2023-05-10 09:02:11 -0600

    update

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -54,7 +54,7 @@ const Checkout = () => {
                              prefix={"$"}
 
 
-                             nnfojoiwijoowwr
+                             
                              />
                         </div>
 
