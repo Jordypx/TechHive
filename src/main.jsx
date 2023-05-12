514479022d87ce9ab0a3412e3f9f0aeb1e75ddc0
jordypx <jordanodo89@gmail.com>
2023-05-11 18:19:48 -0600

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { StateProvider } from './contexts/StateProvider'
-import reducer, { initialState } from './contexts/reducer'
+import reducer, { initialState } from '../reducer'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
