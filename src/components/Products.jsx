9da631f7803a4a4818670dda2d0cbb48dde0512b
jordypx <jordanodo89@gmail.com>
2023-08-05 23:47:29 -0600

    updated

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,15 @@
-import { RiStarSFill } from 'react-icons/ri'
-import { useStateValue } from '../contexts/StateProvider';
+import { RiStarSFill } from "react-icons/ri";
+import { useStateValue } from "../contexts/StateProvider";
 import { useNavigate } from "react-router-dom";
-import mock from "/mock.json"
+import mock from "/mock.json";
 
-const Products = ({ }) => {
-   const [{ basket, baskets }, dispatch] = useStateValue();
-   const navigate = useNavigate();
+const Products = ({}) => {
+  const [{ basket, baskets }, dispatch] = useStateValue();
+  const navigate = useNavigate();
 
-   const addToBasket = (id, title, image, price, rating) => {
+  const addToBasket = (id, title, image, price, rating) => {
     dispatch({
-      type: 'ADD_TO_BASKET',
+      type: "ADD_TO_BASKET",
       item: {
         id: id,
         title: title,
@@ -22,55 +22,83 @@ const Products = ({ }) => {
 
   const ImageInfo = (id, title, image, price, rating, description) => {
     dispatch({
-      type: 'SET_SELECTED_BASKET',
+      type: "SET_SELECTED_BASKET",
       item: {
         id: id,
         title: title,
         image: image,
         price: price,
         rating: rating,
-        description: description
+        description: description,
       },
     });
-    navigate('/imageInfo')
-  }
-  
+    navigate("/imageInfo");
+  };
+
   return (
     <div className="p-9">
       <div className="headNews">
         <h1>HeadPhones For You!</h1>
       </div>
-  
+
       <div className="headphones-container pb-9  mt-3">
         <div className="Image-cards ml-9">
-          {mock.map(post => (
+          {mock.map((post) => (
             <div key={post.id}>
-              <div className="image" onClick={() => ImageInfo(post.id, post.title, post.image, post.price, post.rating, post.description)}>
+              <div
+                className="image"
+                onClick={() =>
+                  ImageInfo(
+                    post.id,
+                    post.title,
+                    post.image,
+                    post.price,
+                    post.rating,
+                    post.description
+                  )
+                }
+              >
                 <img src={post.image} alt="" className="pt-3 cursor-pointer" />
               </div>
-  
+
               <div className="content flex">
                 <div className="description">
                   <h2>{post.title}</h2>
                   <p>{post.description}</p>
                   <div className="reviews flex gap-2">
                     <div className="stars flex">
-                      {Array(post.rating).fill().map((_, i) => (
-                        <RiStarSFill key={i} />
-                      ))}
+                      {Array(post.rating)
+                        .fill()
+                        .map((_, i) => (
+                          <RiStarSFill key={i} />
+                        ))}
                     </div>
                     <span className="">(345)</span>
                   </div>
-  
+
                   <div className="addTocart">
-                    <button onClick={() => addToBasket(post.id, post.title, post.image, post.price, post.rating)} className="mb-8">
+                    <button
+                      onClick={() =>
+                        addToBasket(
+                          post.id,
+                          post.title,
+                          post.image,
+                          post.price,
+                          post.rating
+                        )
+                      }
+                      className="mb-8"
+                    >
                       Add to Cart
                     </button>
                   </div>
                 </div>
-  
+
                 <div className="price">
-                  <span className=""><strong>$</strong>{post.price}</span>
+                  <span className="">
+                    <strong>$</strong>
+                    {post.price}
+                  </span>
                 </div>
               </div>
             </div>
@@ -79,9 +107,6 @@ const Products = ({ }) => {
       </div>
     </div>
   );
-  
+};
 
-     
-}  
-     
-export default Products;                                                                                                                 
+export default Products;
