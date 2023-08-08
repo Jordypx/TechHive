ac7b160af368c4ca28d34fb742caff699cbdaa42
jordypx <jordanodo89@gmail.com>
2023-08-07 19:22:01 -0600

    update

--- src/components/InfoImage.jsx
+++ src/components/InfoImage.jsx
@@ -30,7 +30,6 @@ const InfoImage = ({ id, title, image, price, rating, description }) => {
   };
 
   const handleDecrement = () => {
-    ff;
     if (count > 0) {
       setCount(count - 1);
       dispatch({
